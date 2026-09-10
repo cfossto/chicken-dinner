5e2f3bb99654ae3e4245b303cd3b8d6306ddbcb8
cfossto <70951673+cfossto@users.noreply.github.com>
2026-09-10 23:44:18 +0200

    Data utils now finds best stocks per day.

--- src/chicken_dinner/datautils/datautils.py
+++ src/chicken_dinner/datautils/datautils.py
@@ -1,14 +1,33 @@
+"""
+I wanted to use Pandas here, since it is the best tool for the job.
+Of course, we can do this with the Standardlib as well. But I reasoned
+that Pandas was the right choice.
+
+The thing that differs from stdlib is that I would have used the
+internal csv package for reading and/or writing. I would probably have
+made Pydantic types, dataclasses or pure objects from scratch.
+
+But for this project Pandas is fine.
+"""
+
 import pandas as pd
+from os import path
+
 
 def load_csv(path_to_file: str) -> pd.DataFrame:
     """Loads CSV file at specified path and delivers a Pandas Dataframe"""
-    df = pd.read_csv(path_to_file, delimiter=';')
-    return df
+    if not path.exists(path_to_file):
+        raise FileNotFoundError()
 
-def find_max_last_stock(df: pd.DataFrame) -> pd.DataFrame:
+    return pd.read_csv(path_to_file, delimiter=';')
+
+def find_last_stock_per_day(df: pd.DataFrame) -> pd.DataFrame:
     """Finds latest stock per day at 'closing time' per day"""
-    df.sort_values("date") # Sorting by date and time
+    df.sort_values("Date") # Sorting by date and time
     df['Date'] = df['Date'].astype(str).str[:10] # Makes date unique, not time. Later: Exclude last by date.
-    df.groupby(["Kod", "Date"])["Kurs"].last().reset_index() # Here: Last by date.
+    df = df.groupby(["Kod", "Date"]).last().reset_index() # Here: Last by date.
     return df
 
+def find_winners(df: pd.DataFrame) -> pd.DataFrame:
+    """Finds winners based on stock performance between days"""
+    pass
