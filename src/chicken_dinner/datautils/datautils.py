4ac182ba2030e3d835f8c415d92aa0481552fd9d
cfossto <70951673+cfossto@users.noreply.github.com>
2026-09-13 12:34:55 +0200

    Cleanup. Forgot a print statement used for test.

--- src/chicken_dinner/datautils/datautils.py
+++ src/chicken_dinner/datautils/datautils.py
@@ -37,10 +37,8 @@ def load_csv(path_to_file: str) -> pd.DataFrame:
 
 def _find_last_stock_per_day(df: pd.DataFrame) -> pd.DataFrame:
     """Finds latest stock per day at 'closing time' per day"""
-
     df = df.sort_values("Date") # Sorting by date and time
     pd.to_datetime(df['Date'], format="mixed")
-    print(df)
     df = df.groupby(["Kod", "Date"]).last().reset_index() # Here: Last by date.
     return df
 
